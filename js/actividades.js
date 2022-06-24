c1e0242d2c3368cf003d6bf43e2899d996d49fae
Odriozola <36420107+Odriozola@users.noreply.github.com>
2022-06-24 16:54:43 -0500

    Add files via upload

--- js/actividades.js
+++ js/actividades.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
             //llamamos form oculto en el documento con alertify
             alertify.FormCliente($('#FormAct')[0])
                 .set('position', 'bottom-center')
-                .resizeTo('85%', 400);
+                .resizeTo('85%', 600);
         }
     });
 }); //ready
